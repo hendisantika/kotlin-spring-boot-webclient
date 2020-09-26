f14ffbadaa4f78bace3dd0e55775dd4c043a6f9c
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-27 06:14:04 +0700

    #14 - Add TestController

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootwebclient/controller/TestController.kt
@@ -0,0 +1,17 @@
+package com.hendisantika.kotlinspringbootwebclient.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-webclient
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/09/20
+ * Time: 06.13
+ */
+@RestController
+@RequestMapping("/api")
+class TestController
