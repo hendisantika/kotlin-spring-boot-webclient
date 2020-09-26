7e6af0a2c5b08cf793b2c66d5f943542cde8d9eb
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-27 06:03:35 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootwebclient/KotlinSpringBootWebclientApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.kotlinspringbootwebclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootWebclientApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootWebclientApplication>(*args)
+}
---
+++ src/test/kotlin/com/hendisantika/kotlinspringbootwebclient/KotlinSpringBootWebclientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.kotlinspringbootwebclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootWebclientApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
