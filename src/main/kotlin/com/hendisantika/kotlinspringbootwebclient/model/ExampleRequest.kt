f7c4745b05bc692198d4ab8c4b1869f5fede12e4
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-27 06:09:48 +0700

    #4 - Add ExampleRequest

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootwebclient/model/ExampleRequest.kt
@@ -0,0 +1,14 @@
+package com.hendisantika.kotlinspringbootwebclient.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-webclient
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/09/20
+ * Time: 06.09
+ */
+data class ExampleRequest(
+        val requestMessage: String
+)
