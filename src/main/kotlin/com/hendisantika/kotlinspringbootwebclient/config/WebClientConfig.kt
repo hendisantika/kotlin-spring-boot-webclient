36ef711a206c946f6a0436158664bbf4c7f3d5f8
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-27 06:07:57 +0700

    #3 - Add WebClientConfig

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootwebclient/config/WebClientConfig.kt
@@ -0,0 +1,20 @@
+package com.hendisantika.kotlinspringbootwebclient.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-webclient
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/09/20
+ * Time: 06.05
+ */
+@Configuration
+class WebClientConfig {
+
+    private val logger = LoggerFactory.getLogger(WebClientConfig::class.java)
+
+}
